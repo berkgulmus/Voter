7b3d9b9a76e39ebed7a1caba78a2382daf1739fa
xmbgulmus <mstf.brk.glms@gmail.com>
2024-01-28 16:14:25 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import stylex from '@stylexjs/stylex'
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<script lang="ts">
+const styles = stylex.create({
+  root: {
+    borderRadius: 8,
+    padding: 16,
+    boxShadow: '0 0 16px rgba(0, 0, 0, 0.1)'
+  }
+})
+</script>
+<template>
+  <div :class="stylex(styles.root)">stylex</div>
+
+  <RouterView />
+</template>
